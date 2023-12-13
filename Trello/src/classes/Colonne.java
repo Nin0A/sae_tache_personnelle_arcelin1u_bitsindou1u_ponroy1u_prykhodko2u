30ce019bd03f421b154cffa7b281baa4fd2d8e6d
Maëlle <maelle.bitsindou2@etu.univ-lorraine.fr>
2023-12-13 20:52:21 +0100

    JavaDoc

--- Trello/src/classes/Colonne.java
+++ Trello/src/classes/Colonne.java
@@ -2,15 +2,26 @@ package classes;
 
 import java.util.ArrayList;
 
+//Classe Colonne
 public class Colonne extends Composant<Tache> {
 
+    //constructeur
     Colonne(String n) {
         super(n);
     }
 
+    /**
+     * Méthode ajouterTache qui ajoute une tache à la liste de taches de la colonne
+     * @param t tache à ajouter à la liste
+     */
     public void ajouterTache(Tache t){
         this.liste.add(t);
     }
+
+    /**
+     * Méthode supprimerTache qui supprimer une tache à la liste de taches de la colonne
+     * @param t tache à ajouter à la liste
+     */
     public void supprimerTache(Tache t){
         this.liste.remove(t);
     }
