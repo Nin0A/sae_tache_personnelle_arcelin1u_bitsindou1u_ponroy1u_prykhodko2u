c8d1fb3ab3242f93ca84c759f4d786823783ca1b
Maëlle <maelle.bitsindou2@etu.univ-lorraine.fr>
2023-12-13 16:48:11 +0100

    Modif methode dans Tache

--- Trello/src/classes/Composant.java
+++ Trello/src/classes/Composant.java
@@ -1,16 +1,28 @@
 package classes;
 
 public abstract class Composant {
-    protected String nom;
 
+    //constructeur
+    protected String nom; //nom du composant crée
+
+    //constructeur
     Composant(String n) {
         nom = n;
     }
 
+    /**
+     * Méthode modifierNom qui modifie le nom d'un composant
+     * @param nom nouveau nom modifié du composant
+     */
     public void modifierNom(String nom) {
         this.nom = nom;
     }
 
+    /**
+     * Méthode equals qui compare 2 objets de type Composant
+     * @param o objet avec lequel on compare
+     * @return true si les 2 objets sont pareils, sinon false
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- Trello/src/classes/Tache.java
+++ Trello/src/classes/Tache.java
@@ -3,19 +3,43 @@ package classes;
 import java.util.ArrayList;
 
 public abstract class Tache extends Composant {
-    private double duree;
 
+    //constructeurs
+    private double duree; //durée de la tache
+    private ArrayList<Tache> antecedents; //liste des antécedents de la tache
+
+    /**
+     * Constructeur
+     * @param desc nom de la tache cree
+     * @param duree duree de la tache cree
+     */
     Tache(String desc, int duree) {
         super(desc);
         this.duree = duree;
     }
-    private ArrayList<TacheMere> antecedents;
-    public void  ajouterAntecedent(TacheMere t){
+
+    /**
+     * Méthode ajouterAntecedent qui va permettre d'ajouter une tache
+     * à la liste des antécedents
+     * @param t la tache antécédente à ajouter à la liste
+     */
+    public void  ajouterAntecedent(Tache t){
         this.antecedents.add(t);
     }
-    public void supprimerAntecedent(TacheMere t){
+
+    /**
+     * Méthode supprimerAntecedent qui va permettre de supprimer une tache
+     * à la liste des antécedents
+     * @param t la tache antécédente à supprimer de la liste
+     */
+    public void supprimerAntecedent(Tache t){
         this.antecedents.remove(t);
     }
+
+    /**
+     * Méthide toString qui affiche le nom de la tache et sa duree
+     * @return l'affichage
+     */
     @Override
     public String toString() {
         return this.nom + " "+duree+ " ";
