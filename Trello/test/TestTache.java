08f51cee22990b99a7cd79b9a6f5683de01ef9a3
Maëlle <maelle.bitsindou2@etu.univ-lorraine.fr>
2023-12-17 17:15:13 +0100

    Fin des tests classe Tache

--- Trello/test/TestTache.java
+++ Trello/test/TestTache.java
@@ -27,14 +27,18 @@ public class TestTache {
          t2 = new TacheMere("Tache 2", 10);
          t3 = new TacheMere("Tache 3", 5);
          t4 = new TacheMere("Tache 4", 5);
+
+         //on ajoute les taches 1 et 2 comme antécédents de la tache 3
+        t3.ajouterAntecedent(t1);
+        t3.ajouterAntecedent(t2);
+
+        //on ajoute la tache 3 comme antécédent de la tache 4
+        t4.ajouterAntecedent(t3);
     }
 
     //teste la méthode ajouterAntecedent quand fonctionne correctement
     @Test
     public void test_ajouterAntecedent_OK(){
-        //on ajoute les taches 1 et 2 comme antécédents de la tache 3
-        t3.ajouterAntecedent(t1);
-        t3.ajouterAntecedent(t2);
 
         //vérifier que les antécédents de la tache 3 sont bien les taches 1 et 2
         assertTrue(t3.getAntecedent().contains(t1));
@@ -44,13 +48,6 @@ public class TestTache {
     //test la méthode ajouterAntecedent quand la tache est déjà dans la liste des antécédents
     @Test
     public void test_ajouterAntecedent_2taches_pareilles(){
-        //on ajoute les taches 1 et 2 comme antécédents de la tache 3
-        t3.ajouterAntecedent(t1);
-        t3.ajouterAntecedent(t2);
-
-        //vérifier que les antécédents de la tache 3 sont bien les taches 1 et 2
-        assertTrue(t3.getAntecedent().contains(t1));
-        assertTrue(t3.getAntecedent().contains(t2));
 
         //on ajoute la tache 1 comme antécédent de la tache 3
         t3.ajouterAntecedent(t1);
@@ -62,13 +59,6 @@ public class TestTache {
     //test la méthode supprimerAntecedent quand fonctionne correctement
     @Test
     public void test_supprimerAntecedent_OK(){
-        //on ajoute les taches 1 et 2 comme antécédents de la tache 3
-        t3.ajouterAntecedent(t1);
-        t3.ajouterAntecedent(t2);
-
-        //vérifier que les antécédents de la tache 3 sont bien les taches 1 et 2
-        assertTrue(t3.getAntecedent().contains(t1));
-        assertTrue(t3.getAntecedent().contains(t2));
 
         //on supprime la tache 1 comme antécédent de la tache 3
         t3.supprimerAntecedent(t1);
@@ -80,13 +70,6 @@ public class TestTache {
     //test la méthode supprimerAntecedent quand la tache n'est pas dans la liste des antécédents
     @Test
     public void test_supprimerAntecedent_tache_pas_dans_liste(){
-        //on ajoute les taches 1 et 2 comme antécédents de la tache 3
-        t3.ajouterAntecedent(t1);
-        t3.ajouterAntecedent(t2);
-
-        //vérifier que les antécédents de la tache 3 sont bien les taches 1 et 2
-        assertTrue(t3.getAntecedent().contains(t1));
-        assertTrue(t3.getAntecedent().contains(t2));
 
         //on supprime la tache 4 comme antécédent de la tache 3
         t3.supprimerAntecedent(new TacheMere("Tache 4", 5));
@@ -99,16 +82,6 @@ public class TestTache {
     //teste la methode supprimerAntecedent quand la tache est un antecedent d'une autre tache
     @Test
     public void test_supprimerAntecedent_tache_antecedente(){
-        //on ajoute les taches 1 et 2 comme antécédents de la tache 3
-        t3.ajouterAntecedent(t1);
-        t3.ajouterAntecedent(t2);
-
-        //on ajoute la tache 3 comme antécédent de la tache 4
-        t4.ajouterAntecedent(t3);
-
-        //vérifier que les antécédents de la tache 3 sont bien les taches 1 et 2
-        assertTrue(t3.getAntecedent().contains(t1));
-        assertTrue(t3.getAntecedent().contains(t2));
 
         //vérifier que la tache 3 est bien un antécédent de la tache 4
         assertTrue(t4.getAntecedent().contains(t3));
@@ -126,9 +99,6 @@ public class TestTache {
     //teste la méthode getAntecedent quand fonctionne correctement
     @Test
     public void test_getAntecedent_OK(){
-        //on ajoute les taches 1 et 2 comme antécédents de la tache 3
-        t3.ajouterAntecedent(t1);
-        t3.ajouterAntecedent(t2);
 
         //vérifier que les antécédents de la tache 3 sont bien les taches 1 et 2
         assertTrue(t3.getAntecedent().contains(t1));
@@ -138,8 +108,8 @@ public class TestTache {
     //teste la méthode getAntecedent quand la liste est vide
     @Test
     public void test_getAntecedent_liste_vide(){
-        //vérifier que la liste des antécédents de la tache 3 est vide
-        assertEquals(0, t3.getAntecedent().size());
+        //vérifier que la liste des antécédents de la tache 1 est vide
+        assertEquals(0, t1.getAntecedent().size());
     }
 
 }
