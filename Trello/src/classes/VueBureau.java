156e2c4be97923b35b318d02fbcafcc4d6e204ca
nino.arcelin@gmail.com <101625357+Nin0A@users.noreply.github.com>
2023-12-20 17:44:41 +0100

    merge

--- Trello/src/classes/VueBureau.java
+++ Trello/src/classes/VueBureau.java
@@ -1,6 +1,7 @@
 package classes;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -8,6 +9,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.TextFlow;
 
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
@@ -23,6 +25,8 @@ public class VueBureau extends HBox implements Observateur {
         public void actualiser(Sujet sujet) {
             Tableau tab = (Tableau) sujet;
             this.getChildren().clear();
+            this.setSpacing(10);
+
             /*
             System.out.println("VueBureau : " + tab.getNom());
 
@@ -33,15 +37,16 @@ public class VueBureau extends HBox implements Observateur {
                 System.out.println(colonne.getNom() + " :"+"\t"); //on met un tab entre chaque colonne
             }
             */
-            this.setPadding(new Insets(0,0,0,0));
-            this.setStyle("-fx-border-color: red; -fx-border-width: 2px;");
 
             VBox colonnetmp;
 
             for(int i=0;i<tab.getColonnes().size();i++){
                 colonnetmp= new VBox();
-                colonnetmp.setStyle("-fx-border-color: purple; -fx-border-width: 2px;");
-                colonnetmp.setPadding(new Insets(50));
+                colonnetmp.setSpacing(10);
+                colonnetmp.setMinWidth(250);
+                colonnetmp.setStyle("-fx-border-color: purple; -fx-border-width: 5px;-fx-border-radius: 50px;-fx-background-color: rgb(255,255,255,0.5) ; -fx-background-radius:50px; ");
+                colonnetmp.setPadding(new Insets(20));
+                colonnetmp.setAlignment(Pos.TOP_CENTER);
                 HBox zoneHauteColonne = new HBox();
 
                 Label titreColonne = new Label(tab.getColonnes().get(i).getNom());
@@ -49,24 +54,202 @@ public class VueBureau extends HBox implements Observateur {
                 titreColonne.setStyle("-fx-font-family: 'Arial';" +
                                         "-fx-font-size: 20;" +
                                         "-fx-font-weight: bold;" +
-                                        "-fx-text-fill: blue;");
+                                        "-fx-text-fill: #b40047;" +
+                                "-fx-font-family: Krungthep;");
 
-                zoneHauteColonne.getChildren().addAll(titreColonne,new Button("Modifier"),new Button("Supprimer"));
 
-                colonnetmp.getChildren().addAll(zoneHauteColonne);
+                VBox vbox = new VBox();
+
+                zoneHauteColonne.getChildren().addAll(new Button("Modifier"),new Button("Supprimer"));
+
+                vbox.getChildren().addAll(titreColonne,zoneHauteColonne);
+                zoneHauteColonne.setAlignment(Pos.CENTER);
+                vbox.setPadding(new Insets(20));
+                vbox.setAlignment(Pos.CENTER);
+                colonnetmp.getChildren().addAll(vbox);
                 for(Tache t : tab.getColonnes().get(i).getTaches()){
                     HBox tachetmp = new HBox();
-                    tachetmp.getChildren().addAll(new Label(t.getNom()),new Button("Modifier"),new Button("Archiver"),new Button("Supprimer"));
-                    colonnetmp.getChildren().addAll(tachetmp);
+                    tachetmp.setAlignment(Pos.CENTER);
+
+
+                    Button modifier=new Button("Modifier");
+
+                    Button archiver = new Button("Archiver");
+
+                    Button supprimer = new Button("Supprimer");
+
+                    ////***** Buton modifier //////
+
+                    modifier.setStyle(
+                            "-fx-font-size: 10px; " +
+                            "-fx-padding: 5px; " +
+                            "-fx-background-color: #ffe1fd; " + // Couleur de fond
+                            "-fx-text-fill: #000000; " + // Couleur du texte
+                            "-fx-border-color: #cea1c9; " + // Couleur de la bordure
+                            "-fx-border-width: 2px; "+
+                            "-fx-border-radius: 50px;" + // Bordure arrondie
+                            "-fx-background-radius: 50px;" // Coin arrondi pour le fond
+                    );
+
+                    // Style pour le survol
+                    modifier.setOnMouseEntered(e -> modifier.setStyle(
+                            "-fx-font-size: 10px; " +
+                                    "-fx-padding: 5px; " +
+                                    "-fx-background-color: #fffefe; " + // Nouvelle couleur de fond au survol
+                                    "-fx-text-fill: #000000; " + // Nouvelle couleur du texte au survol
+                                    "-fx-border-color: #ffc3f8; "+
+                                    "-fx-border-width: 2px; "+
+                                    "-fx-border-radius: 50px;" + // Bordure arrondie
+                                    "-fx-background-radius: 50px;"
+                    ));
+
+                    // Style par défaut après le survol
+                    modifier.setOnMouseExited(e -> modifier.setStyle(
+                            "-fx-font-size: 10px; " +
+                                    "-fx-padding: 5px; " +
+                                    "-fx-background-color: #ffe1fd; " + // Retour à la couleur de fond transparente
+                                    "-fx-text-fill: #000000; " + // Retour à la couleur du texte blanche
+                                    "-fx-border-color: #cea1c9; " + // Retour à la couleur de bordure initiale
+                                    "-fx-border-width: 2px; " +
+                                    "-fx-border-radius: 50px;" + // Bordure arrondie
+                                    "-fx-background-radius: 50px;" // Coin arrondi pour le fond
+                    ));
+                    /////////////////////////////////
+
+                    ////***** Buton archiver //////
+
+                    archiver.setStyle(
+                            "-fx-font-size: 10px; " +
+                                    "-fx-padding: 5px; " +
+                                    "-fx-background-color: #ffe1fd; " + // Couleur de fond
+                                    "-fx-text-fill: #000000; " + // Couleur du texte
+                                    "-fx-border-color: #cea1c9; " + // Couleur de la bordure
+                                    "-fx-border-width: 2px; "+
+                                    "-fx-border-radius: 50px;" + // Bordure arrondie
+                                    "-fx-background-radius: 50px;" // Coin arrondi pour le fond
+                    );
+
+                    // Style pour le survol
+                    archiver.setOnMouseEntered(e -> archiver.setStyle(
+                            "-fx-font-size: 10px; " +
+                                    "-fx-padding: 5px; " +
+                                    "-fx-background-color: #fffefe; " + // Nouvelle couleur de fond au survol
+                                    "-fx-text-fill: #000000; " + // Nouvelle couleur du texte au survol
+                                    "-fx-border-color: #ffc3f8; "+
+                                    "-fx-border-width: 2px; "+
+                                    "-fx-border-radius: 50px;" + // Bordure arrondie
+                                    "-fx-background-radius: 50px;"
+                    ));
+
+                    // Style par défaut après le survol
+                    archiver.setOnMouseExited(e -> archiver.setStyle(
+                            "-fx-font-size: 10px; " +
+                                    "-fx-padding: 5px; " +
+                                    "-fx-background-color: #ffe1fd; " + // Retour à la couleur de fond transparente
+                                    "-fx-text-fill: #000000; " + // Retour à la couleur du texte blanche
+                                    "-fx-border-color: #cea1c9; " + // Retour à la couleur de bordure initiale
+                                    "-fx-border-width: 2px; " +
+                                    "-fx-border-radius: 50px;" + // Bordure arrondie
+                                    "-fx-background-radius: 50px;" // Coin arrondi pour le fond
+                    ));
+                    /////////////////////////////////
+
+
+                    ////***** Buton supprimer //////
+
+                    supprimer.setStyle(
+                            "-fx-font-size: 10px; " +
+                                    "-fx-padding: 5px; " +
+                                    "-fx-background-color: #ffe1fd; " + // Couleur de fond
+                                    "-fx-text-fill: #000000; " + // Couleur du texte
+                                    "-fx-border-color: #cea1c9; " + // Couleur de la bordure
+                                    "-fx-border-width: 2px; "+
+                                    "-fx-border-radius: 50px;" + // Bordure arrondie
+                                    "-fx-background-radius: 50px;" // Coin arrondi pour le fond
+                    );
+
+                    // Style pour le survol
+                    supprimer.setOnMouseEntered(e -> supprimer.setStyle(
+                            "-fx-font-size: 10px; " +
+                                    "-fx-padding: 5px; " +
+                                    "-fx-background-color: #fffefe; " + // Nouvelle couleur de fond au survol
+                                    "-fx-text-fill: #000000; " + // Nouvelle couleur du texte au survol
+                                    "-fx-border-color: #ffc3f8; "+
+                                    "-fx-border-width: 2px; "+
+                                    "-fx-border-radius: 50px;" + // Bordure arrondie
+                                    "-fx-background-radius: 50px;"
+                    ));
+
+                    // Style par défaut après le survol
+                    supprimer.setOnMouseExited(e -> supprimer.setStyle(
+                            "-fx-font-size: 10px; " +
+                                    "-fx-padding: 5px; " +
+                                    "-fx-background-color: #ffe1fd; " + // Retour à la couleur de fond transparente
+                                    "-fx-text-fill: #000000; " + // Retour à la couleur du texte blanche
+                                    "-fx-border-color: #cea1c9; " + // Retour à la couleur de bordure initiale
+                                    "-fx-border-width: 2px; " +
+                                    "-fx-border-radius: 50px;" + // Bordure arrondie
+                                    "-fx-background-radius: 50px;" // Coin arrondi pour le fond
+                    ));
+                    /////////////////////////////////
+
+
+                    tachetmp.getChildren().addAll(new Label(t.getNom()),modifier,archiver,supprimer);
+
+                    TextFlow tf = new TextFlow();
+                    tf.getChildren().addAll(tachetmp);
+                    colonnetmp.getChildren().addAll(tf);
                 }
-                colonnetmp.getChildren().addAll(new Button("Ajouter une tâche"));
-                colonnetmp.setMinHeight(810);
+
+                Button ajouterTache = new Button("Ajouter une tâche");
+
+                ////***** Buton supprimer //////
+
+                ajouterTache.setStyle(
+                        "-fx-font-size: 10px; " +
+                                "-fx-padding: 5px; " +
+                                "-fx-background-color: #ffe1fd; " + // Couleur de fond
+                                "-fx-text-fill: #000000; " + // Couleur du texte
+                                "-fx-border-color: #cea1c9; " + // Couleur de la bordure
+                                "-fx-border-width: 2px; "+
+                                "-fx-border-radius: 50px;" + // Bordure arrondie
+                                "-fx-background-radius: 50px;" // Coin arrondi pour le fond
+                );
+
+                // Style pour le survol
+                ajouterTache.setOnMouseEntered(e -> ajouterTache.setStyle(
+                        "-fx-font-size: 10px; " +
+                                "-fx-padding: 5px; " +
+                                "-fx-background-color: #fffefe; " + // Nouvelle couleur de fond au survol
+                                "-fx-text-fill: #000000; " + // Nouvelle couleur du texte au survol
+                                "-fx-border-color: #ffc3f8; "+
+                                "-fx-border-width: 2px; "+
+                                "-fx-border-radius: 50px;" + // Bordure arrondie
+                                "-fx-background-radius: 50px;"
+                ));
+
+                // Style par défaut après le survol
+                ajouterTache.setOnMouseExited(e -> ajouterTache.setStyle(
+                        "-fx-font-size: 10px; " +
+                                "-fx-padding: 5px; " +
+                                "-fx-background-color: #ffe1fd; " + // Retour à la couleur de fond transparente
+                                "-fx-text-fill: #000000; " + // Retour à la couleur du texte blanche
+                                "-fx-border-color: #cea1c9; " + // Retour à la couleur de bordure initiale
+                                "-fx-border-width: 2px; " +
+                                "-fx-border-radius: 50px;" + // Bordure arrondie
+                                "-fx-background-radius: 50px;" // Coin arrondi pour le fond
+                ));
+
+                /////////////////////////////////
+                ajouterTache.setAlignment(Pos.CENTER);
+                colonnetmp.getChildren().addAll(ajouterTache);
+                colonnetmp.setMinHeight(710);
                 this.getChildren().addAll(colonnetmp);
             }
 
 
             VBox ajoutColonne= new VBox();
-            ajoutColonne.setStyle("-fx-border-color: green; -fx-border-width: 2px;");
+            ajoutColonne.setStyle("-fx-border-color: green; -fx-border-width: 5px;-fx-border-radius: 50px");
             ajoutColonne.setPadding(new Insets(50));
             ajoutColonne.getChildren().addAll(new Button("Ajouter Colonne"));
             this.getChildren().addAll(ajoutColonne);
